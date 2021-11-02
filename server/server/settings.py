e7dc6bdb1e38d97a91b6079a68861a7062164bac
W-Mai <1341398182@qq.com>
2021-11-02 22:23:56 +0800

    配置静态文件

--- server/server/settings.py
+++ server/server/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # '/var/www/static/',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
