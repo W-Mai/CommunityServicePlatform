31ab0bb5daa1fc1e4d3fda74ac7327ff275d9296
W-Mai <1341398182@qq.com>
2021-11-15 15:12:27 +0800

    修复：图片链接

--- application/pages/review_page/review_page.js
+++ application/pages/review_page/review_page.js
@@ -1,157 +1,163 @@
+import {img_url} from "../../utils/api";
+
 var app = getApp();
 const db = wx.cloud.database()
 const _ = db.command
 
 Page({
-
-  data:{
-    is_logged_in:false,
-    nameList: [],
-    //初始化数组
-     addPrice: [{
-          Name: "空社团1",
-          Statu:"未报名",
-          id:1
+    data: {
+        is_logged_in: false,
+        imgs: {
+            "whitespace": img_url("ui_imgs", "whitespace.png"),
+            "waiVqs": img_url("ui_imgs", "waiVqs.jpg")
         },
-        {
-          Name: "空社团2",
-          Statu:"未报名",
-          id:2
-        }
-      ],
-        text1:"报名信息填写",
-        text2:"取消报名",
-        help_content:[
-          "单击“主页”底部菜单栏。",
-          "可以分别选择三种分类，单击“相关学生组织”和“学生社团”即可只显示当前分类的详情。",
-          "单击喜欢的社团或组织后，查看完简介，即可单击底部绿色“报名”按钮。",
-          "报名后，已报名的社团或组织会出现在“审核”页面。",
-          "在审核页面“提交报名表”后，等待审核详细结果。"
+        nameList: [],
+        //初始化数组
+        addPrice: [{
+            Name: "空社团1",
+            Statu: "未报名",
+            id: 1
+        },
+            {
+                Name: "空社团2",
+                Statu: "未报名",
+                id: 2
+            }
         ],
-        ASVerifiedList:[],
-      },
-    
-     
-    /****删除*///这个事件有错误
-    async delete(e) {
-      wx.showLoading({
-        title: '取消中……'
-      })
-      console.log(e)
-      let join_id = e.currentTarget.dataset.id
-      // let tmpList = this.data.nameList
-      // tmpList.splice(currentIndex, 1)
-      
-      let user_id = app.globalData.user_Id
-      // console.log(this.data.nameList[currentIndex], userid)
-
-      let res = await wx.cloud.callFunction({
-        name: "register",
-        data:{
-          join_id:join_id,
-          user_id:user_id,
-          op_mode:1
-        }
-      })
-      wx.hideLoading()
-      wx.showToast({
-        title: '取消成功！',
-        duration: 2000,
-        mask:true,
-        success:res=>{
-          this.onShow()
-        }
-      })
-      
+        text1: "报名信息填写",
+        text2: "取消报名",
+        help_content: [
+            "单击“主页”底部菜单栏。",
+            "可以分别选择三种分类，单击“相关学生组织”和“学生社团”即可只显示当前分类的详情。",
+            "单击喜欢的社团或组织后，查看完简介，即可单击底部绿色“报名”按钮。",
+            "报名后，已报名的社团或组织会出现在“审核”页面。",
+            "在审核页面“提交报名表”后，等待审核详细结果。"
+        ],
+        ASVerifiedList: [],
     },
 
-    baoming:function(e){
-      let id = e.currentTarget.dataset.id
-      let name = e.currentTarget.dataset.name
-      let is_verified = app.globalData.is_verified
 
-      console.log(is_verified)
-
-      if(!is_verified){
-        wx.showToast({
-          title:    '请先在个人中心完善信息后再填写报名表',
-          icon:     "none",
-          duration: 2000
+    /****删除*///这个事件有错误
+    async delete(e) {
+        wx.showLoading({
+            title: '取消中……'
         })
-        return
-      }
-      wx.navigateTo({
-        url: `../../pages/bao/bao?id=${id}&name=${name}`,//要跳转到的页面路径
-      })
+        console.log(e)
+        let join_id = e.currentTarget.dataset.id
+        // let tmpList = this.data.nameList
+        // tmpList.splice(currentIndex, 1)
+
+        let user_id = app.globalData.user_Id
+        // console.log(this.data.nameList[currentIndex], userid)
+
+        let res = await wx.cloud.callFunction({
+            name: "register",
+            data: {
+                join_id: join_id,
+                user_id: user_id,
+                op_mode: 1
+            }
+        })
+        wx.hideLoading()
+        wx.showToast({
+            title: '取消成功！',
+            duration: 2000,
+            mask: true,
+            success: res => {
+                this.onShow()
+            }
+        })
+
+    },
+
+    baoming: function (e) {
+        let id = e.currentTarget.dataset.id
+        let name = e.currentTarget.dataset.name
+        let is_verified = app.globalData.is_verified
+
+        console.log(is_verified)
+
+        if (!is_verified) {
+            wx.showToast({
+                title: '请先在个人中心完善信息后再填写报名表',
+                icon: "none",
+                duration: 2000
+            })
+            return
+        }
+        wx.navigateTo({
+            url: `../../pages/bao/bao?id=${id}&name=${name}`,//要跳转到的页面路径
+        })
     },
 
     /**获取输入框信息**/
     async onShow(options) {
-      if(!app.globalData.is_logged_in) {
-        this.setData({
-          is_logged_in:false
-        })
-        return
-      }
-
-      if(app.globalData.user_group == 'teacher'){
-        wx.switchTab({
-          url: '/pages/select_main_page/select_main_page',
-        })
-        return
-      }
-
-      await app.loadJointASList(app.globalData.user_Id)
-      this.setData({
-        is_logged_in:app.globalData.is_logged_in,
-        nameList:app.globalData.user_joint_ids,
-      })
-      if(!this.data.is_logged_in)return;
-      wx.showLoading({
-        title: '通讯中……',
-      })
-      let res = await db.collection("ASInformations")
-      .where({
-        _id:_.in(this.data.nameList)
-      })
-      .field({
-        name : true
-      })
-      .get()
-      console.log(res.data)
-      let tmpList = []
-      let user_id = app.globalData.user_Id
-
-      for(let index in res.data){
-        let element = res.data[index]
-        let appinfo = (await wx.cloud.callFunction({
-          name: "manageApplication",
-          data:{
-            "form_info":{
-                "user_id": user_id,
-                "join_id": element._id
-            },
-            "op":"get"
+        if (!app.globalData.is_logged_in) {
+            this.setData({
+                is_logged_in: false
+            })
+            return
         }
-        })).result
-        console.log(element)
-        tmpList.push(
-          {
-            name : element.name,
-            status : appinfo?appinfo.msg:"未提交报名表",
-            join_id : element._id
-          }
-        )
-      };
 
-      this.setData({
-        ASVerifiedList:tmpList
-      })
-      wx.hideLoading()
+        if (app.globalData.user_group == 'teacher') {
+            wx.switchTab({
+                url: '/pages/select_main_page/select_main_page',
+            })
+            return
+        }
+
+        await app.loadJointASList(app.globalData.user_Id)
+        this.setData({
+            is_logged_in: app.globalData.is_logged_in,
+            nameList: app.globalData.user_joint_ids,
+        })
+        if (!this.data.is_logged_in) return;
+        wx.showLoading({
+            title: '通讯中……',
+        })
+        let res = await db.collection("ASInformations")
+            .where({
+                _id: _.in(this.data.nameList)
+            })
+            .field({
+                name: true
+            })
+            .get()
+        console.log(res.data)
+        let tmpList = []
+        let user_id = app.globalData.user_Id
+
+        for (let index in res.data) {
+            let element = res.data[index]
+            let appinfo = (await wx.cloud.callFunction({
+                name: "manageApplication",
+                data: {
+                    "form_info": {
+                        "user_id": user_id,
+                        "join_id": element._id
+                    },
+                    "op": "get"
+                }
+            })).result
+            console.log(element)
+            tmpList.push(
+                {
+                    name: element.name,
+                    status: appinfo ? appinfo.msg : "未提交报名表",
+                    join_id: element._id
+                }
+            )
+        }
+        ;
+
+        this.setData({
+            ASVerifiedList: tmpList
+        })
+        wx.hideLoading()
     },
-    submitHandler(event){
-      wx.switchTab({
-        url: '../news/news',
-      })
+    submitHandler(event) {
+        wx.switchTab({
+            url: '../news/news',
+        })
     }
 })
